10f9dc7d3aa60835c1bf37797828c27acc17016c
Alexey Kaydash <4tharraofdagon@gmail.com>
2018-06-11 20:01:03 +0300

    adds axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: []
+  };
+
+  myValue = React.createRef();
+
+  getUsers = e => {
+    e.preventDefault();
+    console.log(this.myValue.current.value);
+  };
+
   render() {
     return (
       <div className="App">
-        <p className="App-intro">Hello World</p>
+        <form onSubmit={this.getUsers}>
+          <input type="text" ref={this.myValue} />
+          <button>Get Users</button>
+        </form>
       </div>
     );
   }
